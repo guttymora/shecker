1868872ddf0e1def0e96a59c9b7175379339a11f
Gustavo A Mora P <tavo.mora92@hotmail.com>
2022-02-11 00:31:43 -0400

    new validations for date objects & string dates (onlydates)

--- src/validations.ts
+++ src/validations.ts
@@ -1,3 +1,5 @@
+import { DATE_REGEXES, DATE_VALID_FORMATS } from './constants';
+
 const checkisString = (value: any): string | null => {
     if (!(typeof value === 'string')) {
         return 'Invalid type. Required: string';
@@ -26,6 +28,60 @@ const checkIsObject = (value: any): string | null => {
     if (!(value instanceof Object)) {
         return 'Invalid type. Required: object';
     }
+
+    return null;
+}
+
+const checkIsDateInstance = (value: any): string | null => {
+    if (!(value instanceof Date)) {
+        return 'Invalid type. Required: date';
+    }
+
+    return null;
+}
+
+const checkDateFormat = (value: any, rule: string): string | null => {
+    if (typeof value !== 'string') return 'Invalid type. Required: string';
+
+    // Check if value meet one of the right formats
+    const validDate = DATE_REGEXES.some(regex => regex.test(value));
+    if (!validDate) return 'Invalid date';
+
+    // Check if rule is well-formed
+    let format: string | string[] = rule.split(':')[1];
+    if (!format || format.trim() === '') return 'Invalid date format';
+
+    switch (format) {
+        case DATE_VALID_FORMATS.DD_MM_YYYY:
+            value = value.split('-');
+            value = `${value[2]}-${value[1]}-${value[0]}`;
+            break;
+
+        case DATE_VALID_FORMATS.DD__MM__YYYY:
+            value = value.split('/');
+            value = `${value[2]}/${value[1]}/${value[0]}`;
+            break;
+
+        case DATE_VALID_FORMATS.MM_DD_YYYY:
+            value = value.split('-');
+            value = `${value[2]}-${value[0]}-${value[1]}`;
+            break;
+
+        case DATE_VALID_FORMATS.MM__DD__YYYY:
+            value = value.split('/');
+            value = `${value[2]}/${value[0]}/${value[1]}`;
+            break;
+
+        case DATE_VALID_FORMATS.YYYY_MM_DD:
+        case DATE_VALID_FORMATS.YYYY__MM__DD:
+            break;
+
+        default:
+            return 'Invalid date format';
+    }
+
+    const date = new Date(value);
+    if (!(date instanceof Date) || isNaN(date.getTime())) return 'Invalid date';
 
     return null;
 }
@@ -107,10 +163,12 @@ const getNumberLimit = (rule: string): number | null => {
  * to check if the rule exists and, in that case, return a result of testing the incoming value
  */
 const validations = new Map<any, (value: any, rule?: string) => string | null>();
-validations.set(/string/, (value) => checkisString(value));
-validations.set(/number/, (value) => checkIsNumber(value));
-validations.set(/array/, (value) => checkIsArray(value));
-validations.set(/object/, (value) => checkIsObject(value));
+validations.set(/^string$/, (value) => checkisString(value));
+validations.set(/^number$/, (value) => checkIsNumber(value));
+validations.set(/^array$/, (value) => checkIsArray(value));
+validations.set(/^object$/, (value) => checkIsObject(value));
+validations.set(/^date$/, (value) => checkIsDateInstance(value));
+validations.set(/^onlydate:/, (value, rule) => checkDateFormat(value, rule!));
 validations.set(/^min:/, (value, rule) => checkMin(value, rule!));
 validations.set(/^max:/, (value, rule) => checkMax(value, rule!));
 validations.set(/^size:/, (value, rule) => checkSize(value, rule!));
